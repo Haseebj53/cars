b62eb43cb588b369bee048b8966b9c8c17c58c86
Haseebj53 <haseebj53@gmail.com>
2024-07-30 22:07:02 +1000

    Redux store and Car list functionality

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,5 +1,43 @@
-function CarList(){
-    return <div>Car List</div>
+import { useDispatch, useSelector } from "react-redux";
+import { removeCar } from "../store";
+
+function CarList() {
+  const dispatch = useDispatch();
+  const { cars, name } = useSelector(({ form, cars: { data, searchTerm } }) => {
+    const filteredCars = data.filter((car) =>
+      car.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    return {
+      cars: filteredCars,
+      name: form.name,
+    };
+  });
+  const handleCarDelete = (id) => {
+    dispatch(removeCar(id));
+  };
+  const renderedCars = cars.map((car) => {
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
+
+    return (
+      <div key={car.id} className={`panel ${bold && 'bold'}`}>
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button
+          className="button is-danger"
+          onClick={() => handleCarDelete(car.id)}>
+          Delete
+        </button>
+      </div>
+    );
+  });
+  return (
+    <div className="car-list">
+      {renderedCars}
+      <hr />
+    </div>
+  );
 }
 
 export default CarList;
