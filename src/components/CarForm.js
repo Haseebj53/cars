188b6c8efaa19a4fcaba4fda4dbb5b377db7faca
Haseebj53 <haseebj53@gmail.com>
2024-07-30 20:18:40 +1000

    cars components and store added

---
+++ src/components/CarForm.js
@@ -0,0 +1,17 @@
+function CarForm() {
+  return (
+    <div className="car-form panel">
+      <h4 className="subtitle is-3">Add Car</h4>
+      <form>
+        <div className="field-group">
+          <div className="field">
+            <label>Add Car</label>
+            <input />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default CarForm;
---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  addCar,
+  removeCar,
+  changeSearchTerm,
+  carsReducer,
+} from "./slices/carsSlice";
+import { changeName, changeCost, formReducer } from "./slices/formSlice";
+
+const store = configureStore({
+  reducer: { cars: carsReducer, form: formReducer },
+});
+
+export { store, addCar, removeCar, changeSearchTerm, changeName, changeCost };
